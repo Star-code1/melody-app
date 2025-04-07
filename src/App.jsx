5887203c9be745a64f455096da7a6105891ff685
star-code1 <EmailGitHub>
2025-04-08 02:22:07 +0700

    render

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,38 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.css';
-import { BrowserRouter } from 'react-router-dom'; 
-import { Row, Col } from 'react-bootstrap';
-import Sidebar from './components/Sidebar';
-import Page from './components/Page';
-import Header from './components/Header'
-import Player from './components/Player'
-import { MusicPlayerProvider } from './contexts/MusicPlayerContext';
-import {disableReactDevTools} from '@fvilers/disable-react-devtools';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import { BrowserRouter } from "react-router-dom";
+import { Row, Col } from "react-bootstrap";
+import Sidebar from "./components/Sidebar";
+import Page from "./components/Page";
+import Header from "./components/Header";
+import Player from "./components/Player";
+import { MusicPlayerProvider } from "./contexts/MusicPlayerContext";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 
-if (process.env.NODE_ENV === 'production') {
+// Enable this in production to improve performance and security
+if (process.env.NODE_ENV === "production") {
   disableReactDevTools();
 }
 
 function App() {
-  
   return (
-    <BrowserRouter basename="/"> 
-    <MusicPlayerProvider>
-      <div className='container-fluid vh-100' >
-        <Row>
-          <Col style={{ flex: "0.25", padding: "0" }}>
-            <Sidebar />
-          </Col>
-          <Col  style={{padding: "0"}}>
-            <Header />
-            <Page />
-          </Col>
-        </Row>
-      </div>
-      <Player></Player>
+    <BrowserRouter>
+      <MusicPlayerProvider>
+        <div className="container-fluid vh-100">
+          <Row>
+            <Col style={{ flex: "0.25", padding: "0" }}>
+              <Sidebar />
+            </Col>
+            <Col style={{ padding: "0" }}>
+              <Header />
+              <Page />
+            </Col>
+          </Row>
+        </div>
+        <Player />
       </MusicPlayerProvider>
     </BrowserRouter>
   );
 }
-export default App;
 
+export default App;
